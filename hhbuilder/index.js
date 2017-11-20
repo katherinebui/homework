413cd99c142c612c268dfabc7d444aed26a6cb02
katherinebui <kathy.codes@gmail.com>
2017-11-20 15:00:47 -0800

    Add validation of inputs on form

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -5,3 +5,20 @@ window.addEventListener('load', function() {
     addPeople();
   });
 });
+
+function validateForm() {
+  var age = document.querySelector('[name=age]').value;
+  var relationship = document.querySelector('[name=rel]').value;
+  if (age <= 0 && relationship == '') {
+    alert('Invalid age and relationship input');
+    return false;
+  } else if (age <= 0) {
+    alert('Invalid age');
+    return false;
+  } else if (relationship == '') {
+    alert('Please specify a relationship');
+    return false;
+  } else {
+    return true;
+  }
+}
