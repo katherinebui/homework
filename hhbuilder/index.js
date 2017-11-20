ea1dc03bf21ed882aeb92efffbb410c73286b173
katherinebui <kathy.codes@gmail.com>
2017-11-20 15:03:36 -0800

    Add remove previous button and attached it to function

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -8,6 +8,17 @@ window.addEventListener('load', function() {
   var header = document.createElement('h3');
   var h1 = document.querySelector('h1');
   h1.insertAdjacentHTML('afterend', '<h3>Age, Relationship, Smoker?</h3>');
+
+  var div = document.createElement('div');
+  var removeButton = document.createElement('button');
+  removeButton.className = 'remove';
+  removeButton.innerHTML = 'remove previous item';
+  div.appendChild(removeButton);
+  document.querySelector('form').appendChild(div);
+  removeButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    removeItem();
+  });
 });
 
 function validateForm() {
